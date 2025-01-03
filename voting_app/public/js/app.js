02a75227b1cf8056dad7e5a8801dfa3e765abf73
Riston <104647695+riston996@users.noreply.github.com>
2025-01-03 07:33:33 +0400

    v app2
    updated time 2

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,29 +1,123 @@
+/* eslint-disable no-param-reassign, operator-assignment */
+
 class ProductList extends React.Component {
-    render() {  
-      return (  
-      <div className='ui unstackable items'>  
-        <Product />
-      </div>  
-    );  
-  }  
-} 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    this.handleProductDownVote = this.handleProductDownVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  // Inside `ProductList`
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
+  handleProductDownVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes - 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
+  render() {
+    const products = this.state.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+        onDownVote={this.handleProductDownVote}
+      />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
+      </div>
+    );
+  }
+}
+
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+    this.handleDownVote = this.handleDownVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
+  handleDownVote() {
+    this.props.onDownVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
         <div className='image'>
-          <img src='images/products/image-aqua.png' />
+          <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+            <a onClick={this.handleDownVote}>
+              <i className='large caret down icon' />
+            </a>
+          </div>
           <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='ui avatar image'
-              src='images/avatars/daniel.jpg'
+              src={this.props.submitterAvatarUrl}
             />
           </div>
         </div>
@@ -31,7 +125,6 @@ class Product extends React.Component {
     );
   }
 }
-
 
 ReactDOM.render(
   <ProductList />,
